a122830b666a74b30f2f458226638aa2c2e91e0b
Askin <asknkskk@gmail.com>
2022-07-11 22:10:47 +0300

    followers page

--- src/components/Followers.jsx
+++ src/components/Followers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Paginate from "./Paginate";
+import CardFollowers from "./CardFollowers";
+import loadingGif from "../assets/loading.gif";
+import { useState } from "react";
+import { Container, Row } from "react-bootstrap";
 
-const Followers = () => {
+const Followers = ({ followers }) => {
+  const { loading, followersList } = followers;
+  const [followersPerPage] = useState(12);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastFollower = currentPage * followersPerPage;
+  const indexOffFirstFollower = indexOfLastFollower - followersPerPage;
+  const currentFollowers = followersList.slice(
+    indexOffFirstFollower,
+    indexOfLastFollower
+  );
+  const totalPages = Math.ceil(followersList.length / followersPerPage);
+
   return (
-    <div>Followers</div>
-  )
-}
+    <div>
+      {loading ? (
+        <div>
+          <img src={loadingGif} alt="loading..." />
+        </div>
+      ) : (
+        <div>
+          <Container>
+            <Row xs={2} md={3} lg={4}>
+              {currentFollowers?.map((follower, index) => (
+                <div key={index}>
+                  <CardFollowers follower={follower} />
+                </div>
+              ))}
+            </Row>
+          </Container>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Followers
+export default Followers;
