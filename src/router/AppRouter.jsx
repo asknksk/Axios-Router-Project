aff9b9b020ca11ab6f06f979f66a65eab80b08bc
Askin <asknkskk@gmail.com>
2022-07-11 19:37:20 +0300

    Router added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "../pages/About";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import NavBar from "../components/NavBar";
+import { Nav } from "react-bootstrap";
 
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default AppRouter;
