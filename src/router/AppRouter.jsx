ee684ff73a4c430521cdba62ea2ac951e129af55
Askin <asknkskk@gmail.com>
2022-07-11 19:57:24 +0300

    loginform

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,17 +3,21 @@ import About from "../pages/About";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import NavBar from "../components/NavBar";
-import { Nav } from "react-bootstrap";
+import NotFound from "../pages/NotFound";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <NavBar/>
-    <Routes>
-        <Route path="/" element={<Home/>}/>
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 };
 
 export default AppRouter;
